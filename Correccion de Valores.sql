67a259f9ca68595f0e39efcbb71e2bfe0f1c4ae0
MatiasLopezperlo23 <matiaslopezperlo2023@gmail.com>
2024-10-16 22:49:45 -0300

    Correcion de Valores de la tabla Player_stats

---
+++ Correccion de Valores.sql
@@ -0,0 +1,50 @@
+USe Sportics_LAM
+
+Select * from player_stats
+
+UPDATE player_stats
+SET _2PA = 1
+WHERE Rk = 73;
+
+UPDATE player_stats
+SET FTA = 3
+WHERE Rk = 176;
+
+UPDATE player_stats
+SET FGA = 4
+WHERE Rk = 200;
+
+UPDATE player_stats
+SET _2PA = 6
+WHERE Rk = 403;
+
+UPDATE player_stats
+SET _2PA = 4
+WHERE Rk = 407;
+
+UPDATE player_stats
+SET _2PA = 2
+WHERE Rk = 448;
+
+UPDATE player_stats
+SET FGA = 9
+WHERE Rk = 85;
+
+UPDATE player_stats
+SET FTA = 1
+WHERE Rk = 190;
+
+UPDATE player_stats
+SET _2PA = 1
+WHERE Rk = 211;
+
+UPDATE player_stats
+SET _2PA = 1
+WHERE Rk = 360;
+
+UPDATE player_stats
+SET _2PA = 7
+WHERE Rk = 482;
+
+
+
--- automatizacion_Sportics.sql
+++ automatizacion_Sportics.sql
@@ -1,4 +1,4 @@
 USE Sportics_LAM;
 --codigo para ejecutar las query de automatizacion
-
+use Sportics_LAM
 EXEC AUTOMATIZACION_GENERAL_SLAM;
